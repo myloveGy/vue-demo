54787469098d59d1cd8d3788dd4a7bd9b4397356
liujinxing <jinxing.liu@verystar.cn>
2019-05-10 16:01:24 +0800

    feat: 项目修改

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,42 +1,43 @@
 <template>
-<div>
+  <div>
     <div class="page-header">
-        <h1 v-html="title"></h1>
+      <h1 v-html="title"></h1>
     </div>
     <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-        <item v-for="(list, key) in lists" :data="list" :key="key"/>
+      <item v-for="(list, key) in lists" :data="list" :key="key"/>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
-    import Item from './Item'
-    export default {
-        name: "index",
-        data() {
-            return {
-                title: "我的Web服务器 <small> 选择需要进行的操作 </small>",
-            }
-        },
-        components: {
-            Item
-        },
-        computed: {
-            lists() {
-                if (this.$store.state.lists.length === 0) {
-                    this.$http.get(this.Config.serverHost, {
-                        params: {
-                            action: "index",
-                        }
-                    }).then(function(response) {
-                        if (response.body.code === 0) {
-                            this.$store.commit("setLists", response.body.data);
-                        }
-                    });
-                }
+  import Item from './Item'
 
-                return this.$store.state.lists;
+  export default {
+    name: 'index',
+    data() {
+      return {
+        title: '我的Web服务器 <small> 选择需要进行的操作 </small>',
+      }
+    },
+    components: {
+      Item,
+    },
+    computed: {
+      lists() {
+        if (this.$store.state.lists.length === 0) {
+          this.$http.get(this.Config.serverHost, {
+            params: {
+              action: 'index',
+            },
+          }).then(function (response) {
+            if (response.body.code === 0) {
+              this.$store.commit('setLists', response.body.data)
             }
+          })
         }
-    }
+
+        return this.$store.state.lists
+      },
+    },
+  }
 </script>
